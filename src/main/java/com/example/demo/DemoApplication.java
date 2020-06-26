5ea03bfaa91d6588b352698073cdb231456deefe
Administrator <wukivision@gmail.com>
2020-06-26 15:20:23 +0800

    first commit 1

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -16,6 +16,7 @@ public class DemoApplication {
 
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "world") String name) {
+
         return String.format("Hello %s", name);
     }
 
